edef7c41bb3b32c6c8307cd2fe5f0dd18ca0fa0c
Travcort <tarvkoome@gmail.com>
2024-11-07 23:05:39 +0300

    Registering Post Model in Admin Interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import BlogPost
 
-# Register your models here.
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    search_fields = ['title']
+    ordering = ['-created_at']
+
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
