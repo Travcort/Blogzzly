c13068ac3f9b730a1e77425362a654e085a2ae33
Travcort <tarvkoome@gmail.com>
2024-11-14 23:29:42 +0300

    Version 1.0

--- my_site/settings.py
+++ my_site/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('HOSTS').split(',')
 
 
 # Application definition
